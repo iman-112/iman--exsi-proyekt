550267306df75b81436056b30f5921dd2fe77b24
first_iman <iman.xeliov@mail.ru>
2022-06-02 13:09:00 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const searchBtn = document.querySelector('search-Btn');
+
+function funksiya (e){
+searchBtn.addEventListener('<i class="fa-solid fa-magnifying-glass-location"></i>')
+
+}
+console.log(funksiya);
